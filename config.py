2fa1dfd29de17e1a272dd16b74a1f3b6e56e1b5c
LiYingwei <liyw14@163.com>
2018-04-23 13:39:12 -0400

    support FGSM

--- config.py
+++ config.py
@@ -16,19 +16,21 @@ config = edict()
 config.random_range = 0.1  # will be change to 0.0 if mode is eval
 config.attack_network = "resnet_v2_152"
 config.pgd = False
+config.FGSM = False
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument("--random_range", type=float, default=config.random_range)
 parser.add_argument("--attack_network", type=str, default=config.attack_network)
 parser.add_argument("--pgd", type=bool, default=config.pgd)
+parser.add_argument("--FGSM", type=bool, default=config.FGSM)
 
 args = parser.parse_args()
 for key, value in args.__dict__.iteritems():
     config[key] = value
 
 config.max_epsilon = 15.0 / 255
-config.step_size = 1.0 / 255
-config.num_steps = int(min(config.max_epsilon * 255 + 4, 1.25 * config.max_epsilon * 255))
+config.step_size = 1.0 / 255 if not config.FGSM else config.max_epsilon
+config.num_steps = int(min(config.max_epsilon * 255 + 4, 1.25 * config.max_epsilon * 255)) if not config.FGSM else 1
 config.report_step = 100
 
 config.test_network = ["inception_v3", "inception_v4", "inception_resnet_v2", "resnet_v2_152", "ens3_inception_v3",
@@ -38,11 +40,12 @@ config.ground_truth_file = '../data/FlorianProject/valid_gt.csv'
 config.test_img_dir = '../data/FlorianProject/test_data/'
 config.checkpoint_path = os.path.join(os.path.dirname(__file__), 'data')
 
-
-if not config.pgd:
-    config.result_dir = 'result3/I-FGSM_{:s}_{:.3f}'.format(config.attack_network, config.random_range)
-else:
+if config.pgd:
     config.result_dir = 'result3/PGD_{:s}_{:.3f}'.format(config.attack_network, config.random_range)
+elif config.FGSM:
+    config.result_dir = 'result3/FGSM_{:s}_{:.3f}'.format(config.attack_network, config.random_range)
+else:
+    config.result_dir = 'result3/I-FGSM_{:s}_{:.3f}'.format(config.attack_network, config.random_range)
 
 if eval_mode == 1:
     config.random_range = 0.0
@@ -51,5 +54,7 @@ else:
     config.batch_size = 32
     if not os.path.exists(config.result_dir):
         os.makedirs(config.result_dir)
+    else:
+        assert 0
 
 print(config)
