eff803d1e08dc3190d77fd35df384571f3270f86
LiYingwei <liyw14@163.com>
2018-05-01 10:25:40 -0400

    remove some redundant code

--- networks/lib/lib_resnet_v2_152.py
+++ networks/lib/lib_resnet_v2_152.py
@@ -127,9 +127,7 @@ def bottleneck(inputs,
 
         random_range = FLAGS.random_range if FLAGS.random_range >= -0.5 else 0.11
         weight = tf.random_uniform((depth,), minval=1 - random_range, maxval=1 + random_range)
-        # output = weight * shortcut + (2 - weight) * residual
         output = weight * shortcut + residual
-        # output = (1 - random_range) * shortcut + residual
 
         return utils.collect_named_outputs(outputs_collections, sc.name, output)
 
